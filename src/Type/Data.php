99093ff6319bbc8b0b9a268c5c3baeb8da717dd0
stevenmarkgraf <saferserver@gmail.com>
2015-02-02 15:44:29 +0100

    add type class and context

--- src/Type/Data.php
+++ src/Type/Data.php
@@ -4,4 +4,23 @@ use Lilie\DataObject;
 
 class Data extends DataObject {
 
+    protected $data = [
+
+        /**
+         * Der Name dieses Seitentypes
+         *
+         * @var	string
+         **/
+        'name' => "",
+
+
+        /**
+         * Die Dateien zu diesem Seitentyp
+         *
+         * @var	array
+         **/
+        'files' => []
+
+    ];
+
 }
--- src/Type/Type.php
+++ src/Type/Type.php
@@ -1,6 +1,70 @@
 <?php namespace Lilie\Type;
 
+use Illuminate\Contracts\Foundation\Application as App;
 
 class Type {
+
+    /**
+     * Laravel's application
+     *
+     * @var	    \Illuminate\Contracts\Foundation\Application
+     **/
+    protected $app;
+
+
+    /**
+     * The data for this class.
+     *
+     * @var	    \Lilie\Type\Data
+     **/
+    private $context;
+
+
+    /**
+     * Build a type object.
+     */
+    public function __construct($name, $files, App $app)
+    {
+        $this->app = $app;
+
+        $this->context = $app->make(Data::class, [[
+            'name' => $name,
+            'files' => $files
+        ]]);
+    }
+
+
+    /**
+     * Return the Laravel-App.
+     *
+     * @return  \Illuminate\Contracts\Foundation\Application
+     */
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+
+    /**
+     * Return the context for this object.
+     *
+     * @return  \Lilie\Type\Data
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+
+    /**
+     * Return the content from the context.
+     *
+     * @param	string	$key
+     * @return	mixed
+     **/
+    public function __get($key)
+    {
+        return array_get( $this->context, $key );
+    }
 
 }
