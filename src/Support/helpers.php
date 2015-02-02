cbeecd27bb13edb3c672687df8f88dac1018bde0
stevenmarkgraf <saferserver@gmail.com>
2015-02-02 15:27:58 +0100

    add a little helper: array_undot

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -2,7 +2,7 @@
 
 if( ! function_exists('lilie_path'))
 {
-    /*
+    /**
      * Get the path to the Lilie-Package.
      *
      * @param   string  $path
@@ -11,5 +11,27 @@ if( ! function_exists('lilie_path'))
     function lilie_path($path = '')
     {
         return Config::get('packages.lilie.path') . ($path ?  DIRECTORY_SEPARATOR . $path : $path);
+    }
+}
+
+
+if( ! function_exists('array_undot'))
+{
+    /**
+     * The reverse to array_dot.
+     *
+     * @param   array   $data
+     * @return  array
+     */
+    function array_undot(array $data)
+    {
+        $res = array();
+
+        foreach($data as $key => $item)
+        {
+            array_set($res, $key, $item);
+        }
+
+        return $res;
     }
 }
